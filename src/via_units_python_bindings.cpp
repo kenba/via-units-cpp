a859cfd00c02c7d7da6d4382b7411e9a1990801b
Ken Barker <ken.barker@via-technology.aero>
2024-12-06 20:42:11 +0000

    Update pythion bindings.

--- src/via_units_python_bindings.cpp
+++ src/via_units_python_bindings.cpp
@@ -44,6 +44,8 @@ PYBIND11_MODULE(via_units, m) {
   m.attr("METRES_PER_NAUTICAL_MILE") =
       via::units::non_si::METRES_PER_NAUTICAL_MILE<double>;
   m.attr("METRES_PER_FOOT") = via::units::non_si::METRES_PER_FOOT<double>;
+  m.attr("METRES_PER_SECOND_TO_KNOTS") =
+      via::units::non_si::METRES_PER_SECOND_TO_KNOTS<double>;
 
   // SI Units
 
@@ -56,6 +58,106 @@ PYBIND11_MODULE(via_units, m) {
 
       .def("v", &via::units::si::Metres<double>::v)
       .def("__repr__", &via::units::si::Metres<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the MetresPerSecond class
+  PYBIND11_NUMPY_DTYPE(via::units::si::MetresPerSecond<double>, v_);
+
+  // Python bindings for the MetresPerSecond class
+  py::class_<via::units::si::MetresPerSecond<double>>(m, "MetresPerSecond")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::MetresPerSecond<double>::v)
+      .def("__repr__", &via::units::si::MetresPerSecond<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the MetresPerSecondSquared class
+  PYBIND11_NUMPY_DTYPE(via::units::si::MetresPerSecondSquared<double>, v_);
+
+  // Python bindings for the MetresPerSecondSquared class
+  py::class_<via::units::si::MetresPerSecondSquared<double>>(
+      m, "MetresPerSecondSquared")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::MetresPerSecondSquared<double>::v)
+      .def("__repr__",
+           &via::units::si::MetresPerSecondSquared<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the Kelvin class
+  PYBIND11_NUMPY_DTYPE(via::units::si::Kelvin<double>, v_);
+
+  // Python bindings for the Kelvin class
+  py::class_<via::units::si::Kelvin<double>>(m, "Kelvin")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::Kelvin<double>::v)
+      .def("__repr__", &via::units::si::Kelvin<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the Pascals class
+  PYBIND11_NUMPY_DTYPE(via::units::si::Pascals<double>, v_);
+
+  // Python bindings for the Pascals class
+  py::class_<via::units::si::Pascals<double>>(m, "Pascals")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::Pascals<double>::v)
+      .def("__repr__", &via::units::si::Pascals<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the Kilograms class
+  PYBIND11_NUMPY_DTYPE(via::units::si::Kilograms<double>, v_);
+
+  // Python bindings for the Kilograms class
+  py::class_<via::units::si::Kilograms<double>>(m, "Kilograms")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::Kilograms<double>::v)
+      .def("__repr__", &via::units::si::Kilograms<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the KilogramsPerCubicMetre class
+  PYBIND11_NUMPY_DTYPE(via::units::si::KilogramsPerCubicMetre<double>, v_);
+
+  // Python bindings for the KilogramsPerCubicMetre class
+  py::class_<via::units::si::KilogramsPerCubicMetre<double>>(
+      m, "KilogramsPerCubicMetre")
+      .def(py::init<double>())
+
+      .def("v", &via::units::si::KilogramsPerCubicMetre<double>::v)
+      .def("__repr__",
+           &via::units::si::KilogramsPerCubicMetre<double>::python_repr)
 
       .def(py::self < py::self)
       .def(py::self <= py::self)
@@ -94,6 +196,25 @@ PYBIND11_MODULE(via_units, m) {
       .def("to_metres", &via::units::non_si::Feet<double>::to_metres)
       .def("v", &via::units::non_si::Feet<double>::v)
       .def("__repr__", &via::units::non_si::Feet<double>::python_repr)
+
+      .def(py::self < py::self)
+      .def(py::self <= py::self)
+      .def(py::self > py::self)
+      .def(py::self >= py::self)
+      .def(py::self == py::self);
+
+  // Python numpy binding for the Knots class
+  PYBIND11_NUMPY_DTYPE(via::units::non_si::Knots<double>, v_);
+
+  // Python bindings for the Knots class
+  py::class_<via::units::non_si::Knots<double>>(m, "Knots")
+      .def(py::init<double>())
+      .def(py::init<via::units::si::MetresPerSecond<double>>())
+
+      .def("to_metres",
+           &via::units::non_si::Knots<double>::to_metres_per_second)
+      .def("v", &via::units::non_si::Knots<double>::v)
+      .def("__repr__", &via::units::non_si::Knots<double>::python_repr)
 
       .def(py::self < py::self)
       .def(py::self <= py::self)
